d16841e5d176da3caa847b8d3ad46f06db3ea72f
Rufina Agayev <78618462+rufina83@users.noreply.github.com>
2022-01-26 00:18:53 +0200

    init

---
+++ backend/data.js
@@ -0,0 +1,86 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Cat-eye glasses',
+            category: 'Glasses',
+            image:'/images/p1.jpeg',
+            price: 120,
+            countInStock: 10,
+            brand: 'versach',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+
+        {
+            _id:'2',
+            name:'Smart Glasses',
+            category: 'Glasses',
+            image:'/images/p2.jpeg',
+            price: 130,
+            countInStock: 20,
+            brand: 'Max Mara',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id:'3',
+            name:'Sun glasses',
+            category: 'Glasses',
+            image:'/images/p3.jpg',
+            price: 220,
+            countInStock: 0,
+            brand: 'versach',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id:'4',
+            name:'Max Mara Glasses Tortoise',
+            category: 'Glasses',
+            image:'/images/p4.jpeg',
+            price: 170,
+            countInStock: 15,
+            brand: 'Max Mara',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id:'5',
+            name:'Office glasses',
+            category: 'Glasses',
+            image:'/images/p5.jpeg',
+            price: 140,
+            countInStock: 5,
+            brand: 'Dolce & Gabbana',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id:'6',
+            name:'Max Mara screen glasses',
+            category: 'Glasses',
+            image:'/images/p6.jpeg',
+            price: 110,
+            countInStock: 12,
+            brand: 'Max Mara',
+            rating: 4,
+            numReviews: 8,
+            description: 'high quality product',
+            
+        },
+        
+    ],
+};
+
+export default data;
