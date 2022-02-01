bf28fe4aec3ac2bf93d9ff3e5a374fd649713373
Rufina Agayev <78618462+rufina83@users.noreply.github.com>
2022-02-01 14:44:43 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ import data from './data.js';
 
 const app = express();
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if(product) {
+        res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product not Found'});  
+    }
+});
+
 app.get('/api/products', (req, res) =>{
     res.send(data.products);
 });
@@ -12,5 +21,5 @@ app.get('/', (req, res) => {
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
-    console.log(`Server at http://localhost:${port}`);
+    console.log(`Serve at http://localhost:${port}`);
 });
